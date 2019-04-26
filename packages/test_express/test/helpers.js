e19383d52a336868b9046135a74e82f7eed4d67d
Christopher Radek <cjradek@gmail.com>
2019-04-26 13:46:52 -0700

    bugfix: fixes issue where cls-hooked would overwrite cls namespace

--- packages/test_express/test/helpers.js
+++ packages/test_express/test/helpers.js
@@ -2,6 +2,7 @@ var dgram = require('dgram');
 var xray = require('aws-xray-sdk-core');
 var xrayExpress = require('aws-xray-sdk-express');
 var express = require('express');
+require('cls-hooked'); // validates compat with continuation-local-storage
 var http = require('http');
 var assert = require('chai').assert;
 
@@ -13,61 +14,61 @@ var assert = require('chai').assert;
  * @param {string} options.route
  */
 function createAppWithRoute(options) {
-  var app = express();
-  if (options.dynamicPattern) {
-    xray.middleware.enableDynamicNaming(options.dynamicPattern);
-  }
-  app.use(xrayExpress.openSegment(options.name));
-  app.use(options.route, options.handler);
-  app.use(xrayExpress.closeSegment());
-  return app;
+    var app = express();
+    if (options.dynamicPattern) {
+        xray.middleware.enableDynamicNaming(options.dynamicPattern);
+    }
+    app.use(xrayExpress.openSegment(options.name));
+    app.use(options.route, options.handler);
+    app.use(xrayExpress.closeSegment());
+    return app;
 }
 
 function createDaemon() {
-  return dgram.createSocket('udp4');
+    return dgram.createSocket('udp4');
 }
 
 function messageCounter(expectedCount, callback) {
-  var messages = [];
-  return (message) => {
-    messages.push(message);
-    if (messages.length == expectedCount) {
-      return callback(messages);
-    } 
-  }
+    var messages = [];
+    return (message) => {
+        messages.push(message);
+        if (messages.length == expectedCount) {
+            return callback(messages);
+        }
+    }
 }
 
 function jitter() {
-  return 100 * Math.random();
+    return 100 * Math.random();
 }
 
 function parseMessage(message) {
-  message = message.toString();
-  var parts = message.split('\n');
-  assert.equal(parts.length, 2, 'Daemon message should contain a protocol and a segment document.');
-  // parse the Segment document
-  return JSON.parse(parts[1]);
+    message = message.toString();
+    var parts = message.split('\n');
+    assert.equal(parts.length, 2, 'Daemon message should contain a protocol and a segment document.');
+    // parse the Segment document
+    return JSON.parse(parts[1]);
 }
 
 /**
  * @param {number} ms 
  */
 function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function sleepDedupe() {
-  var inflightSleep = null;
-  return () => {
-    if (!inflightSleep) {
-      inflightSleep = sleep(200);
-      inflightSleep.then(() => {
-        inflightSleep = null
-      });
-    }
+    var inflightSleep = null;
+    return () => {
+        if (!inflightSleep) {
+            inflightSleep = sleep(200);
+            inflightSleep.then(() => {
+                inflightSleep = null
+            });
+        }
 
-    return inflightSleep;
-  }
+        return inflightSleep;
+    }
 }
 
 /**
@@ -75,23 +76,23 @@ function sleepDedupe() {
  * @param {number} waitFor Amount of time to wait before making request in ms
  */
 function triggerEndpoint(url, waitFor) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      http.get(url, (response) => {
-        var chunks = [];
-        response.on('data', (chunk) => {
-          chunks.push(chunk);
-        });
-  
-        response.on('end', () => {
-          resolve({
-            body: Buffer.concat(chunks).toString(),
-            status: response.statusCode
-          });
-        });
-      }).on('error', reject);
-    }, waitFor || 0);
-  });
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            http.get(url, (response) => {
+                var chunks = [];
+                response.on('data', (chunk) => {
+                    chunks.push(chunk);
+                });
+
+                response.on('end', () => {
+                    resolve({
+                        body: Buffer.concat(chunks).toString(),
+                        status: response.statusCode
+                    });
+                });
+            }).on('error', reject);
+        }, waitFor || 0);
+    });
 }
 
 /**
@@ -103,24 +104,24 @@ function triggerEndpoint(url, waitFor) {
  * @param {string} expectedFields.url Express route url
  */
 function validateExpressSegment(segment, expectedFields) {
-  assert.equal(segment.name, expectedFields.name);
-  assert.isNumber(segment.end_time);
-  assert.isNumber(segment.start_time);
-  assert.equal(segment.http.request.method, 'GET');
-  assert.isString(segment.http.request.client_ip);
-  assert.equal(segment.http.request.url, expectedFields.url);
-  assert.equal(segment.http.response.status, expectedFields.responseStatus);
-  assert.isString(segment.trace_id);
+    assert.equal(segment.name, expectedFields.name);
+    assert.isNumber(segment.end_time);
+    assert.isNumber(segment.start_time);
+    assert.equal(segment.http.request.method, 'GET');
+    assert.isString(segment.http.request.client_ip);
+    assert.equal(segment.http.request.url, expectedFields.url);
+    assert.equal(segment.http.response.status, expectedFields.responseStatus);
+    assert.isString(segment.trace_id);
 }
 
 module.exports = {
-  createAppWithRoute: createAppWithRoute,
-  createDaemon: createDaemon,
-  jitter: jitter,
-  messageCounter: messageCounter,
-  parseMessage: parseMessage,
-  sleep: sleep,
-  sleepDedupe: sleepDedupe,
-  triggerEndpoint: triggerEndpoint,
-  validateExpressSegment: validateExpressSegment
+    createAppWithRoute: createAppWithRoute,
+    createDaemon: createDaemon,
+    jitter: jitter,
+    messageCounter: messageCounter,
+    parseMessage: parseMessage,
+    sleep: sleep,
+    sleepDedupe: sleepDedupe,
+    triggerEndpoint: triggerEndpoint,
+    validateExpressSegment: validateExpressSegment
 };
