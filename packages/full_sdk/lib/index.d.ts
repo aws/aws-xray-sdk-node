600686013c9826756b1a597326866e86795e6436
William Armiros <armiros@amazon.com>
2020-10-29 09:39:41 -0600

    fixed mysql import in full_sdk

--- packages/full_sdk/lib/index.d.ts
+++ packages/full_sdk/lib/index.d.ts
@@ -1,5 +1,5 @@
 import * as express from 'aws-xray-sdk-express';
-import captureMySQL from 'aws-xray-sdk-mysql';
+import { captureMySQL } from 'aws-xray-sdk-mysql';
 import capturePostgres from 'aws-xray-sdk-postgres';
 
 export * from 'aws-xray-sdk-core';
