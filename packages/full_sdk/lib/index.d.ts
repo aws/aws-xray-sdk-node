ed723813971ee29694771fb033cfccab276fd843
William Armiros <armiros@amazon.com>
2020-10-29 08:33:58 -0600

    switched full sdk to es6 syntax

--- packages/full_sdk/lib/index.d.ts
+++ packages/full_sdk/lib/index.d.ts
@@ -1,6 +1,6 @@
 import * as express from 'aws-xray-sdk-express';
-import captureMySQL = require('aws-xray-sdk-mysql');
-import capturePostgres = require('aws-xray-sdk-postgres');
+import captureMySQL from 'aws-xray-sdk-mysql';
+import capturePostgres from 'aws-xray-sdk-postgres';
 
 export * from 'aws-xray-sdk-core';
 
--- packages/full_sdk/test-d/index.test-d.ts
+++ packages/full_sdk/test-d/index.test-d.ts
@@ -1,6 +1,6 @@
 import * as express from 'aws-xray-sdk-express';
-import captureMySQL = require('aws-xray-sdk-mysql');
-import capturePostgres = require('aws-xray-sdk-postgres');
+import captureMySQL from 'aws-xray-sdk-mysql';
+import capturePostgres from 'aws-xray-sdk-postgres';
 import { expectType } from 'tsd';
 import * as AWSXRay from '../lib';
 
