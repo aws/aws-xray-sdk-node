9407138926909d5bfbcc3fa68a97029905dc1c52
Tucker Whitehouse <TuckerWhitehouse@gmail.com>
2020-11-16 15:12:40 -0500

    Update .d.ts files to reflect 'downstreamXRayEnabled' is optional (#357)

--- packages/core/lib/patchers/http_p.d.ts
+++ packages/core/lib/patchers/http_p.d.ts
@@ -4,6 +4,6 @@ import { Subsegment } from '../aws-xray';
 
 type httpSubsegmentCallback = (subsegment: Subsegment, req: http.ClientRequest, res: http.IncomingMessage | null, error: Error) => void
 
-export function captureHTTPs<T extends typeof http | typeof https>(mod: T, downstreamXRayEnabled: boolean, subsegmentCallback?: httpSubsegmentCallback): T;
+export function captureHTTPs<T extends typeof http | typeof https>(mod: T, downstreamXRayEnabled?: boolean, subsegmentCallback?: httpSubsegmentCallback): T;
 
-export function captureHTTPsGlobal(mod: typeof https | typeof http, downstreamXRayEnabled: boolean, subsegmentCallback?: httpSubsegmentCallback): void;
+export function captureHTTPsGlobal(mod: typeof https | typeof http, downstreamXRayEnabled?: boolean, subsegmentCallback?: httpSubsegmentCallback): void;
