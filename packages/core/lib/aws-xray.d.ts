511fa971d5eded1328106337885d37426d4d5114
William Armiros <armiros@amazon.com>
2021-04-12 19:08:21 -0500

    Revert "Revert PR #386 (#412)"
    This reverts commit 9e20d2e880d706baa9ef8041ae70989f2f932c04.

--- packages/core/lib/aws-xray.d.ts
+++ packages/core/lib/aws-xray.d.ts
@@ -39,7 +39,7 @@ export { captureAsyncFunc, captureCallbackFunc, captureFunc } from './capture'
 
 export { captureAWS, captureAWSClient } from './patchers/aws_p';
 
-export function captureAWSv3Client<T>(client: T, manualSeg?: SegmentLike): T;
+export type { captureAWSClient as captureAWSv3Client } from './patchers/aws3_p';
 
 export { captureHTTPs, captureHTTPsGlobal } from './patchers/http_p';
 
