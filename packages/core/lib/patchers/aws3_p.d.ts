6d4d0a3c01afbddadd483560e8d14d16afc46113
William Armiros <armiros@amazon.com>
2021-05-05 11:11:15 -0700

    updated deps and type file

--- packages/core/lib/patchers/aws3_p.d.ts
+++ packages/core/lib/patchers/aws3_p.d.ts
@@ -1,12 +1,11 @@
-import { Pluggable, Client, MetadataBearer } from '@aws-sdk/types';
+import { Client, MetadataBearer } from '@aws-sdk/types';
 import { RegionResolvedConfig } from '@aws-sdk/config-resolver';
 import { SegmentLike } from '../aws-xray';
-export declare const getXRayPlugin: (config: RegionResolvedConfig, manualSegment?: SegmentLike) => Pluggable<any, any>;
 /**
- * Original API to capture an AWS SDK V3, it is now deprecated and acts as a no-op.
+ * Instruments AWS SDK V3 clients with X-Ray via middleware.
  *
  * @param client - AWS SDK V3 client to instrument
  * @param manualSegment - Parent segment or subsegment that is passed in for manual mode users
- * @returns - the original client
+ * @returns - the client with the X-Ray instrumentation middleware added to its middleware stack
  */
 export declare function captureAWSClient<Input extends object, Output extends MetadataBearer, Configuration extends RegionResolvedConfig>(client: Client<Input, Output, Configuration>, manualSegment?: SegmentLike): Client<Input, Output, Configuration>;
