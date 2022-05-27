47fa4001f71e9dad4029b72183f0151d932209f3
Nathaniel Ruiz Nowell <enowell@amazon.com>
2022-05-26 18:13:13 -0700

    Remove support for Node 10.x

--- packages/postgres/lib/postgres_p.js
+++ packages/postgres/lib/postgres_p.js
@@ -111,12 +111,6 @@ function captureQuery() {
 
       var errorCapturer = function (err) {
         subsegment.close(err);
-
-        // TODO: Remove this logic once Node 10 is deprecated
-        if (!events.errorMonitor && this.listenerCount('error') <= 1) {
-          this.removeListener('error', errorCapturer);
-          this.emit('error', err);
-        }
       };
 
       query.on(events.errorMonitor || 'error', errorCapturer);
