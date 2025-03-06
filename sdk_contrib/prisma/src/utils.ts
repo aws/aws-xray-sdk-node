cca81e7dbc735831858581332b515cef1d1bb99c
Eduard Castellanos <andrescastellanos112013@gmail.com>
2025-03-06 12:29:44 +1000

    fix(prisma): Update @prisma/client to version 5.22.0 and refactor ModelAction enum in utils.ts

--- sdk_contrib/prisma/src/utils.ts
+++ sdk_contrib/prisma/src/utils.ts
@@ -1,7 +1,25 @@
-import { DMMF } from '@prisma/client/runtime/binary';
 import { PrismaPromise } from './types';
 
-const { ModelAction } = DMMF;
+enum ModelAction {
+  findUnique = 'findUnique',
+  findUniqueOrThrow = 'findUniqueOrThrow',
+  findFirst = 'findFirst',
+  findFirstOrThrow = 'findFirstOrThrow',
+  findMany = 'findMany',
+  create = 'create',
+  createMany = 'createMany',
+  createManyAndReturn = 'createManyAndReturn',
+  update = 'update',
+  updateMany = 'updateMany',
+  upsert = 'upsert',
+  delete = 'delete',
+  deleteMany = 'deleteMany',
+  groupBy = 'groupBy',
+  count = 'count', // TODO: count does not actually exist, why?
+  aggregate = 'aggregate',
+  findRaw = 'findRaw',
+  aggregateRaw = 'aggregateRaw',
+}
 
 export const isAction = (action: any): action is keyof typeof ModelAction =>
   !!ModelAction[action as keyof typeof ModelAction];
